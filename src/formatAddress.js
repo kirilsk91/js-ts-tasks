df1532dbe0aac52ffd143c5b0239769c60c303c1
kirils.kravcovs <kirils.kravcovs@abcsoftware.lv>
2024-11-19 13:46:55 +0200

    feat: solve task 2

--- src/formatAddress.js
+++ src/formatAddress.js
@@ -4,5 +4,5 @@
  * @returns {function}
  */
 module.exports.formatAddress = function formatAddress() {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return ({ street, house, apartment, city, postalCode, country }) => `${street}, ${house}, ${apartment}, ${city}, ${postalCode}, ${country}`;
 };

e7f8ca2dfd27c48db7206d05c7a20bafb15ffba3
kirils.kravcovs <kirils.kravcovs@abcsoftware.lv>
2024-11-19 09:25:05 +0200

    fix: update diffElems solution

--- src/diffElems.js
+++ src/diffElems.js
@@ -4,13 +4,18 @@
  * @returns {number}
  */
 module.exports.diffElems = function diffElems(arr) {
-  let i = 0;
-  const elArr = [];
-  while (i < arr.length) {
-    if (!elArr.includes(arr[i])) {
-      elArr.push(arr[i]);
-    }
-    i += 1;
-  }
-  return elArr.length;
+  // Naive solution with high complexity.
+  // We can use Set to handle duplicate items in array and return array size.
+  // let i = 0;
+  // const elArr = [];
+  // while (i < arr.length) {
+  //   if (!elArr.includes(arr[i])) {
+  //     elArr.push(arr[i]);
+  //   }
+  //   i += 1;
+  // }
+  // return elArr.length;
+  const result = new Set(arr);
+
+  return result.size;
 };

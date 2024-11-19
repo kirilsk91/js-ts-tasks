79ef07606d4748d05f297020bdf9990f9ade227c
kirils.kravcovs <kirils.kravcovs@abcsoftware.lv>
2024-11-19 14:45:26 +0200

    feat: solve task 4

--- src/primeNumbers.js
+++ src/primeNumbers.js
@@ -3,6 +3,31 @@
  * @param {number} highestNumber - a highest possible number
  * @returns {function}
  */
+// eslint-disable-next-line no-unused-vars
 module.exports.primeNumbers = function primeNumbers(highestNumber) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  // Not sure where to use highestNumber parameter
+  const primesArray = [];
+
+  const isPrime = num => {
+    // Natural numbers (greater than 1) can be prime numbers, except
+    // when they are even (excluding 2)
+    if (num === 1 || (num % 2 === 0 && num !== 2)) return false;
+    const sqrtNum = Math.sqrt(num);
+    for (let i = 2; i <= sqrtNum; i++) {
+      if (num % i === 0) return false;
+    }
+
+    return true;
+  };
+
+  return (num1, num2) => {
+    const low = Math.min(num1, num2);
+    const high = Math.max(num1, num2);
+    let i = low;
+    while (i <= high) {
+      if (isPrime(i)) primesArray.push(i);
+      i += 1;
+    }
+    return primesArray;
+  };
 };

25f1d4793715be240315eb130ea47c2c16e7c256
Kirils Kravcovs <kravcovs.kirils@student.ehu.lt>
2025-01-23 15:09:44 +0200

    feat: solve tasks 1 and 2

--- src/converter.ts
+++ src/converter.ts
@@ -5,6 +5,38 @@
  * @param {'m'|'mi'|'gr'|'pound'|'C'|'K'} to
  * @returns {boolean}
  */
+// eslint-disable-next-line func-names
 module.exports.converter = function (value: number, from: string, to: string): number {
-  throw new Error('Not implemented'); // delete this line and write your code
+  const mToMiConst = 0.000621371;
+  const miToMConst = 1609.34;
+  const grToLbConst = 0.00220462;
+  const lbToGrConst = 453.592;
+  const tempConst = 273.15;
+
+  let res: number;
+
+  switch (`${from}=>${to}`) {
+    case 'mi=>m':
+      res = value * miToMConst;
+      break;
+    case 'm=>mi':
+      res = value * mToMiConst;
+      break;
+    case 'gr=>pound':
+      res = value * grToLbConst;
+      break;
+    case 'pound=>gr':
+      res = value * lbToGrConst;
+      break;
+    case 'C=>K':
+      res = value + tempConst;
+      break;
+    case 'K=>C':
+      res = value - tempConst;
+      break;
+    default:
+      throw new Error(`Conversion isn't possible with ${from} and ${to}.`);
+  }
+
+  return parseFloat(res.toFixed(2));
 };
--- src/getDaysToNewYear.ts
+++ src/getDaysToNewYear.ts
@@ -1,8 +1,31 @@
 /**
  * Write a function to calculate the days left until the next New Year's eve (In 2023 the next NY is 1th January, 2024 year)
- * @param {Date | string} targetDate
+ * @param {Date | string} dateToCheck
  * @returns {number}
  */
-module.exports.getDaysToNewYear = function getDaysToNewYear(targetDate: Date | string): number {
-  throw new Error('Not implemented'); // delete this line and write your code
+module.exports.getDaysToNewYear = function getDaysToNewYear(dateToCheck: Date | string): number {
+  const isLeapYear = (year: number): boolean =>
+    (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+  const convertDate = (date: Date | string): Date => {
+    if (typeof date === 'string') {
+      const [day, month, year] = date.split('.');
+      const formattedDate = `${year}-${month}-${day}`;
+
+      return new Date(formattedDate);
+    }
+
+    return date;
+  };
+
+  let targetYear: number = convertDate(dateToCheck).getFullYear();
+
+  while (!isLeapYear(targetYear)) {
+    targetYear += 1;
+  }
+
+  const timeDifference =
+    new Date(`${targetYear}-01-01`).getTime() - convertDate(dateToCheck).getTime();
+
+  return Math.floor(timeDifference / (1000 * 3600 * 24));
 };
